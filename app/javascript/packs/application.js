27a0e08e819a4275f52788ae0bd68b6fb5a851ee
shuhei <>
2021-02-08 09:08:10 +0900

    jsファイルにjqueryをインポート

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -19,7 +18,10 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
+import $ from 'jquery'
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.alert('DOM')
+    $('.profilePage_user_image').on('click', () => {
+        window.alert('clicked')
+    })
 })
