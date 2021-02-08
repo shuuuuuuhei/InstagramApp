5506048441a7abff8290d1e429b1c33725e39abd
shuhei <>
2021-02-08 09:03:21 +0900

    jqueryをインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.alert('DOM')
+})
