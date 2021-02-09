91150dd4298954e5557b0281c6e37e6f2324efde
shuhei <>
2021-02-09 15:17:04 +0900

    アクションを定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,42 @@
 class ArticlesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    
+    def index
+        @articles = Article.all
+    end
     def show
 
+    end
+
+    def new
+        @article = current_user.articles.build
+    end
+
+    def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+            binding.pry
+            edirect_to article_path(@article), notice: '保存できたよ'
+            
+        else
+            flash.now[:error] = '保存に失敗しました'
+        end
+    end
+    
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(
+            :caption,
+            :eyecatch
+        )
     end
 end
