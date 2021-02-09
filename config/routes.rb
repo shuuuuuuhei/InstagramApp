9db4685664b115e8796632b58bfc8a726b314149
shuhei <>
2021-02-09 14:53:32 +0900

    routesの設定 コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'articles#index'
 
+  resources :articles, only: [:create, :show, :edit, :update]
   resource :profile, only: [:show, :edit, :update, :create]
 end
