982768d30cc2c9beec0117cef2d98ef00eaed95d
shuhei <>
2021-02-12 13:16:32 +0900

    いいねした時としていない時の表示を変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,7 +8,10 @@ class LikesController < ApplicationController
     end
 
     def destroy
-        
+        article = Article.find(params[:article_id])
+        like = article.likes.find_by!(user_id: current_user.id)
+        like.destroy!
+        redirect_to root_path
     end
 
     def show
