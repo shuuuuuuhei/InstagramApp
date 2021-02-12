3808b3fe9610d7a0c4d89bb59ce103455b8780a0
shuhei <>
2021-02-12 15:29:36 +0900

    いいねをajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,18 +4,21 @@ class LikesController < ApplicationController
     def create
         article = Article.find(params[:article_id])
         like = article.likes.create!(user_id: current_user.id)
-        redirect_to root_path
+        render json: { status: 'ok' }
     end
 
     def destroy
         article = Article.find(params[:article_id])
         like = article.likes.find_by!(user_id: current_user.id)
         like.destroy!
-        redirect_to root_path
+        render json: { status: 'ok' }
     end
 
+    #ajax
     def show
-
+        article = Article.find(params[:article_id])
+        like_status = current_user.has_liked?(article)
+        render json: { hasLiked: like_status }
     end
 
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,8 +20,56 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
 
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+const handleHeartDisplay = (hasLiked) => {
+    if (hasLiked) {
+        $('.active-heart').removeClass('hidden')
+    }
+    else {
+        $('.inactive-heart').removeClass('hidden')
+    }
+}
 document.addEventListener('DOMContentLoaded', () => {
+    const dataset = $('#article-show').data()
+    const articleId = dataset.articleId
+    axios.get(`/articles/${articleId}/like`)
+        .then((response) => {
+            const hasLiked = response.data.hasLiked
+            handleHeartDisplay(hasLiked)
+            console.log(response)
+        })
+    
+    $('.active-heart').on('click', ()=> {
+        axios.delete(`/articles/${articleId}/like`)
+            .then((response) => {
+                if(response.data.status === 'ok') {
+                    $('.inactive-heart').removeClass('hidden')
+                    $('.active-heart').addClass('hidden')
+                }
+            })
+            .catch((e) => {
+                window.alert('error')
+                console.log(e)
+            })
+    })
+
+    $('.inactive-heart').on('click', ()=> {
+        axios.post(`/articles/${articleId}/like`)
+        .then((response) => {
+            if(response.data.status === 'ok') {
+                $('.active-heart').removeClass('hidden')
+                $('.inactive-heart').addClass('hidden')
+            }
+        })
+        .catch((e) => {
+            window.alert('error')
+            console.log(e)
+        })
+    })
+    
     $('.profilePage_user_image').on('click', () => {
         $('.profilePage_user_image_area').removeClass('hidden')
     })
@@ -79,4 +127,5 @@ document.addEventListener('DOMContentLoaded', () => {
         var obj = document.getElementById('img-file')
         obj.value = ''
     })
+
 })
